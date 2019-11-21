7e49780faeba652353a6de234ce30db5eadcb3fa
KaMinu78 <klym.andriy14@mail.ru>
2019-11-21 16:39:58 +0000

    final

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+$('.multi-carousel .carousel-item').each(function() {
+    let next = $(this).next();
+    if (!next.length) next = $(this).siblings(':first');
+    next.children(':first-child').clone().appendTo($(this));
+  });
+  $('.multi-carousel .carousel-item').each(function() {
+    let prev = $(this).prev();
+    if (!prev.length) prev = $(this).siblings(':last');
+    prev.children(':nth-last-child(2)').clone().prependTo($(this));
+  });
+
 const cart = new Cart($('#cartModal'));
 const productList = new ProductList(
   'products.json',
